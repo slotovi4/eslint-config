f056577cc11bea7ef79ba293d2ab58a1406771c6
Bruhns Roman <roman.bruns@mail.ru>
2022-01-02 15:11:28 +0300

    refactor: refactor eslint for ts/js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,122 +1,29 @@
 module.exports = {
-	parser: "@typescript-eslint/parser",
-	extends: [
-		"plugin:@typescript-eslint/recommended"
-	],
 	plugins: [
-		"@typescript-eslint",
 		"import"
 	],
-	parserOptions: {
-		"ecmaVersion": 2020,
-		"sourceType": "module",
-		"project": "./tsconfig.json"
-	},
 	rules: {
-		"import/no-extraneous-dependencies": ["error", { "devDependencies": false, "optionalDependencies": false, "peerDependencies": false }],
-		"@typescript-eslint/explicit-function-return-type": "off",
-		"@typescript-eslint/interface-name-prefix": "off",
-		"@typescript-eslint/explicit-module-boundary-types": "off",
-		"@typescript-eslint/no-non-null-assertion": "error",
-		"@typescript-eslint/no-use-before-define": "off",
-		"@typescript-eslint/no-shadow": "error",
-		"@typescript-eslint/no-duplicate-imports": "error",
-		"@typescript-eslint/no-extra-semi": "off",
-		"@typescript-eslint/member-ordering": "error",
-		"@typescript-eslint/member-delimiter-style": "error",
-		"@typescript-eslint/switch-exhaustiveness-check": "error",
-		"@typescript-eslint/consistent-type-imports": ["error", { "prefer": "type-imports" }],
-		"@typescript-eslint/explicit-member-accessibility": [
+		"no-multi-spaces": "error",
+		"no-shadow": "off",
+		"no-unused-expressions": "error",
+		"no-unused-labels": "error",
+		"no-extra-semi": "error",
+		"no-param-reassign": "error",
+		"no-use-before-define": "off",
+		"no-duplicate-case": "error",
+		"no-fallthrough": "error",
+		"curly": "error",
+		"object-shorthand": "error",
+		"prefer-arrow-callback": "error",
+		"radix": "error",
+		"class-methods-use-this": "error",
+		"eqeqeq": "error",
+		"import/no-extraneous-dependencies": [
 			"error",
 			{
-				"overrides": {
-					"properties": "off",
-					"parameterProperties": "off"
-				}
-			}
-		],
-		"@typescript-eslint/naming-convention": [
-			"error",
-			{
-				"selector": "default",
-				"format": [
-					"PascalCase",
-					"camelCase"
-				]
-			},
-			// правило для булевых констант
-			{
-				"selector": "variable",
-				"modifiers": [
-					"const"
-				],
-				"types": [
-					"boolean"
-				],
-				"format": [
-					"PascalCase"
-				],
-				"prefix": [
-					"is",
-					"should",
-					"has",
-					"can",
-					"did",
-					"will"
-				]
-			},
-			// правило для массивов
-			{
-				"selector": "variable",
-				"types": [
-					"array"
-				],
-				"format": [
-					"camelCase"
-				],
-				"suffix": [
-					"List"
-				]
-			},
-			// правило для type
-			{
-				"selector": "typeAlias",
-				"format": [
-					"PascalCase"
-				],
-				"prefix": [
-					"T"
-				]
-			},
-			// правило для interface
-			{
-				"selector": "interface",
-				"format": [
-					"PascalCase"
-				],
-				"prefix": [
-					"I"
-				]
-			},
-			// правило для enum
-			{
-				"selector": "enum",
-				"format": [
-					"PascalCase"
-				],
-				"prefix": [
-					"E"
-				]
-			},
-			// правило для переменных класса
-			{
-				"selector": "classProperty",
-				"format": [
-					"camelCase"
-				],
-				"prefix": [
-					"_"
-				]
+				"devDependencies": false,
+				"optionalDependencies": false,
+				"peerDependencies": false
 			}
 		],
 		"valid-jsdoc": [
@@ -138,7 +45,7 @@ module.exports = {
 		"max-lines": [
 			"error",
 			{
-				"max": 500
+				"max": 150
 			}
 		],
 		"max-params": [
@@ -180,20 +87,129 @@ module.exports = {
 				"SwitchCase": 1
 			}
 		],
-		"no-multi-spaces": "error",
-		"no-shadow": "off",
-		"no-unused-expressions": "error",
-		"no-unused-labels": "error",
-		"no-extra-semi": "error",
-		"no-param-reassign": "error",
-		"no-use-before-define": "off",
-		"no-duplicate-case": "error",
-		"no-fallthrough": "error",
-		"curly": "error",
-		"object-shorthand": "error",
-		"prefer-arrow-callback": "error",
-		"radix": "error",
-		"class-methods-use-this": "error",
-		"eqeqeq": "error"
-	}
+	},
+	"overrides": [
+		{
+			parser: "@typescript-eslint/parser",
+			extends: [
+				"plugin:@typescript-eslint/recommended"
+			],
+			plugins: [
+				"@typescript-eslint",
+			],
+			parserOptions: {
+				"ecmaVersion": 2020,
+				"sourceType": "module",
+				"project": "./tsconfig.json"
+			},
+			files: ["*.ts", "*.tsx"],
+			rules: {
+				"@typescript-eslint/explicit-function-return-type": "off",
+				"@typescript-eslint/interface-name-prefix": "off",
+				"@typescript-eslint/explicit-module-boundary-types": "off",
+				"@typescript-eslint/no-non-null-assertion": "error",
+				"@typescript-eslint/no-use-before-define": "off",
+				"@typescript-eslint/no-shadow": "error",
+				"@typescript-eslint/no-duplicate-imports": "error",
+				"@typescript-eslint/no-extra-semi": "off",
+				"@typescript-eslint/member-ordering": "error",
+				"@typescript-eslint/member-delimiter-style": "error",
+				"@typescript-eslint/switch-exhaustiveness-check": "error",
+				"@typescript-eslint/consistent-type-imports": ["error", { "prefer": "type-imports" }],
+				"@typescript-eslint/explicit-member-accessibility": [
+					"error",
+					{
+						"overrides": {
+							"properties": "off",
+							"parameterProperties": "off"
+						}
+					}
+				],
+				"@typescript-eslint/naming-convention": [
+					"error",
+					{
+						"selector": "default",
+						"format": [
+							"PascalCase",
+							"camelCase"
+						]
+					},
+					// правило для булевых констант
+					{
+						"selector": "variable",
+						"modifiers": [
+							"const"
+						],
+						"types": [
+							"boolean"
+						],
+						"format": [
+							"PascalCase"
+						],
+						"prefix": [
+							"is",
+							"should",
+							"has",
+							"can",
+							"did",
+							"will"
+						]
+					},
+					// правило для массивов
+					{
+						"selector": "variable",
+						"types": [
+							"array"
+						],
+						"format": [
+							"camelCase"
+						],
+						"suffix": [
+							"List"
+						]
+					},
+					// правило для type
+					{
+						"selector": "typeAlias",
+						"format": [
+							"PascalCase"
+						],
+						"prefix": [
+							"T"
+						]
+					},
+					// правило для interface
+					{
+						"selector": "interface",
+						"format": [
+							"PascalCase"
+						],
+						"prefix": [
+							"I"
+						]
+					},
+					// правило для enum
+					{
+						"selector": "enum",
+						"format": [
+							"PascalCase"
+						],
+						"prefix": [
+							"E"
+						]
+					},
+					// правило для переменных класса
+					{
+						"selector": "classProperty",
+						"format": [
+							"camelCase"
+						],
+						"prefix": [
+							"_"
+						]
+					}
+				],
+			}
+		}
+	]
 }
